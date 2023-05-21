bc40d91cfe4867ac67fc2e99c182289b2a597246
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-21 22:01:32 +0300

    21.05.2023

---
+++ src/test/java/day16_notations/C03_Priority.java
@@ -0,0 +1,32 @@
+package day16_notations;
+
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C03_Priority extends TestBase {
+    /*
+        TestNG test methodlarini isim sirasina gore calistirir
+        eger isim siralamasinin disinda bir siralama ile calismasini isterseniz
+        o zaman test methodlarina oncelik yani priority tanimlayabiliriz
+
+        priority kucukten buyuge calisir
+        eger bir test methoduna priority degeri atanmamissa default olarak priority=0 kabul edilir.
+     */
+    @Test(priority = -5)
+    public void amazonTest() {
+        driver.get("https://www.amazon.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @Test(priority = -2)
+    public void test02() {
+        driver.get("https://www.bestbuy.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+
+    @Test
+    public void facebookTest() {
+        driver.get("https://www.facebook.com");
+        System.out.println(driver.getCurrentUrl());
+    }
+}
