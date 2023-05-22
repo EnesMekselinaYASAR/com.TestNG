c7c833a1673fa5451c69bb569e9fb2e3d27fe849
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-22 11:45:08 +0300

    22.05.2023

---
+++ src/test/java/day17_pom/C01_YeniDriverIlkClass.java
@@ -0,0 +1,4 @@
+package day17_pom;
+
+public class C01_YeniDriverIlkClass {
+}
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -21,6 +21,6 @@ public abstract class TestBase {
 
     @AfterMethod
     public void tearDown(){
-        //driver.close();
+        driver.close();
     }
 }
