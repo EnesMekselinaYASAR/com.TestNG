d4ebd345521b4bc53478d7cb5868b3f4fdbd2248
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-05-25 09:39:12 +0300

    25.05.2023

--- src/test/java/tests/day21_reusableMethods_HTMLReports/C01_WindowHandleReusableMethods.java
+++ src/test/java/tests/day21_reusableMethods_HTMLReports/C01_WindowHandleReusableMethods.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 import java.util.Set;
 
@@ -30,13 +31,16 @@ public class C01_WindowHandleReusableMethods {
         //açılan yeni tab'in title'ının "New Window" olduğunu test edin.
         String expectedTitle = "New Window";
         String actualTitle = Driver.getDriver().getTitle();
-        Assert.assertEquals(expectedTitle, actualTitle);
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 
     @Test
     public void test02() {
         Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
         Driver.getDriver().findElement(By.linkText("Click Here")).click();
-
+        ReusableMethods.switchToWindow("New Window");
+        String expectedTitle = "New Window";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
     }
 }
